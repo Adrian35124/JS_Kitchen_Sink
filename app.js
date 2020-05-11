125fdc404c817d18f4c436f581b5e485abee2d00
adrian35124 <adrian35124@gmail.com>
2020-05-11 14:44:13 -0500

    FInally Finished

--- app.js
+++ app.js
@@ -1,26 +1,94 @@
 let firstname = "Adrian";
-let states = 50;
-let checkAge= 20
+const states = 50;
+let age = 20;
 console.log(5 + 4);
 //  function helloWorld(alert: "Hello World!");
 
 function name() {
   alert("hello world");
-} if  (checkAge < 21) {
+}
+if (age < 21) {
   alert("Sorry " + firstname + ", you aren't old enough to view this page!");
 }
 
 let list = ["Brocoli", "Corn", "Potatoes", "Cauliflower", "Spinach"];
-console.log(list[0])
-console.log(list[1])
-console.log(list[2])
-console.log(list[3])
-console.log(list[4])
+for (let i = 0; i < list.length; i++) {
+  console.log(list[i]);
+}
 
-let pets = [];
-pets["Name" ] = "Panda";
-pets["breed" ] = "Havense";
-console.log(pets);
+let myPet = {
+  name: "Panda",
+  breed: "Havense",
+};
+console.log(myPet);
 
-let person = ["Jon", "Brian", "Jose", "Adrian", "Jessica"]
- 
+let person = [
+  {
+    name: "Joe",
+    age: 20,
+  },
+  {
+    name: "Josh",
+    age: 26,
+  },
+  {
+    name: "Jessica",
+    age: 24,
+  },
+  {
+    name: "Brian",
+    age: 21,
+  },
+  {
+    name: "Cris",
+    age: 24,
+  },
+];
+
+function checkAge(name, age) {
+  if (age <= 21) {
+    alert("Sorry " + name + ", you aren't old enough to view this page!");
+  } else {
+    alert("Welcome!");
+  }
+}
+
+checkAge("Charles", 21);
+
+let people = [
+  {
+    name: "Josh",
+    age: 26
+  },
+  {
+    name: "Adrian",
+    age: 24
+  },
+  {
+    name: "Kid",
+    age: 400
+  },
+  {
+    name: "KidB",
+    age: 19
+  },
+  {
+    name: "Person1",
+    age: 30
+  },
+];
+
+for (let i = 0; i < people.length; i++) {
+  checkAge(people[i].name, people[i].age);
+}
+
+function getLength(word) {
+  return word.length;
+}
+let words = getLength("Hello World");
+
+if (words % 2 == 0) {
+  console.log("The world is nice and even!");
+} else {
+  console.log("The world is an odd place.");
+}
