8d60285dafac9384958e9dbaa905a162a9213b67
adrian35124 <adrian35124@gmail.com>
2020-05-11 10:54:45 -0500

    Currently Working on it

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 let firstname = "Adrian";
 let states = 50;
-
+let checkAge= 20
 console.log(5 + 4);
 //  function helloWorld(alert: "Hello World!");
 
 function name() {
   alert("hello world");
+} if  (checkAge < 21) {
+  alert("Sorry " + firstname + ", you aren't old enough to view this page!");
 }
 
-name();
+let list = ["Brocoli", "Corn", "Potatoes", "Cauliflower", "Spinach"];
+console.log(list[0])
+console.log(list[1])
+console.log(list[2])
+console.log(list[3])
+console.log(list[4])
+
+let pets = [];
+pets["Name" ] = "Panda";
+pets["breed" ] = "Havense";
+console.log(pets);
+
+let person = ["Jon", "Brian", "Jose", "Adrian", "Jessica"]
+ 
